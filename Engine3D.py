726d58d35ca3cbb6dc589e4b317beeb6cda89345
luis2 <luis212urbina@gmail.com>
2020-07-19 19:21:14 -0600

    glLine works fine

--- Engine3D.py
+++ Engine3D.py
@@ -1,24 +1,25 @@
 from gl import Render, color
 
-r = Render()
-r.glCreateWindow(20,10)
+r = Render(600,600)
 
-r.glColor(0,1,1)
+r.glViewport(100,100, 400, 400)
 
-r.glClearColor(1,0,0)
-    
+r.glClearColor(0,0,0)
+r.glColor(0.45,0.5,0)
 r.glClear()
 
-r.glViewPort(3,1,10, 4)
+r.glLine(-1,0, 0,1)
+r.glLine(0,1, 1,0)
+r.glLine(1,0, 0,-1)
+r.glLine(0,-1, -1,0)
+r.glLine(-1,0, 1,0)
+r.glLine(0,-1, 0,1)
+r.glLine(-1,-1, -1,1)
+r.glLine(-1,1, 1,1)
+r.glLine(1,1, 1,-1)
+r.glLine(1,-1, -1,-1)
+r.glLine(-1,-1, 1,1)
+r.glLine(1,-1, -1,1)
 
-
-r.glVertex(-1,-1)
-r.glVertex(1,1)
-r.glVertex(-1,1)
-r.glVertex(0,0)
-r.glVertex(1,-1)
-r.glVertex(-1,0)
-r.glVertex(1,0)
 
 r.glFinish('output.bmp')
-
--- gl.py
+++ gl.py
@@ -26,23 +26,24 @@ BLACK = color(0,0,0)
 WHITE = color(255,255,255)
 
 class Render(object):
-    def __init__(self):
+    def __init__(self, width, height):
         self.curr_color = WHITE
         self.clear_color = BLACK
+        self.glCreateWindow(width, height)
 
     def glCreateWindow(self, width, height):
         self.width = width
         self.height = height
         self.glClear()
-        self.glViewPort(0,0, width, height)
+        self.glViewport(0,0, width, height)
 
     def glClear(self):
         self.pixels = [ [ self.clear_color for x in range(self.width)] for y in range(self.height) ]
 
-    def glViewPort(self, x, y, width, height):
+    def glViewport(self, x, y, width, height):
         self.viewport_initial_x = x
         self.viewport_initial_y = y
-        self.viewport_witdth = width
+        self.viewport_width = width
         self.viewport_height = height
         self.viewport_final_x = x + width
         self.viewport_final_y = x + height
@@ -53,24 +54,63 @@ class Render(object):
             y >= self.viewport_initial_y and
             y <= self.viewport_final_y)
 
-
     def glClearColor(self, r,g,b):
         rgb_array = decimalToRgb([r,g,b])
         self.clear_color = color(rgb_array[0], rgb_array[1], rgb_array[2])
 
     def glVertex(self, x, y):
-        # v_x = 0
-        # v_y = 0
         if(x >= 0 or x < 0):
-            pixelX = int(( x + 1 ) * ( self.viewport_witdth / 2 ) + self.viewport_initial_x)
+            pixelX = int(( x + 1 ) * ( self.viewport_width / 2 ) + self.viewport_initial_x)
         if(y >= 0 or y < 0):
             pixelY = int(( y + 1 ) * (self.viewport_height /2 ) + self.viewport_initial_y)
         if(self.glVertextInViewport(pixelX,pixelY) == True):
             self.pixels[pixelY][pixelX] = self.curr_color
-
+    
+    def glVertex_coord(self, x, y):
+        self.pixels[y][x] = self.curr_color
+    
     def glColor(self, r,g,b):
         rgb_array = decimalToRgb([r,g,b])
         self.curr_color = color(rgb_array[0], rgb_array[1], rgb_array[2])
+
+    def glFixCoordinate(self, value, main_axis):
+        fixed_coordinate = 0
+        if main_axis:
+            fixed_coordinate = (value+1) * (self.viewport_width/2) + self.viewport_initial_x
+        else:
+            fixed_coordinate = (value+1) * (self.viewport_height/2) + self.viewport_initial_y
+        return round(fixed_coordinate)
+    
+
+    def glLine(self, x0, y0, x1, y1) :
+        x0 = self.glFixCoordinate(x0, True)
+        x1 = self.glFixCoordinate(x1, True)
+        y0 = self.glFixCoordinate(y0, False)
+        y1 = self.glFixCoordinate(y1, False)
+
+        steep = abs(y1 - y0) > abs(x1 - x0)
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx, dy = abs(x1 - x0), abs(y1 - y0)      
+        
+        offset = 0
+        limit =  0.5
+        y = y0
+
+        for x in range(x0, x1+1):
+            self.glVertex_coord(y, x) if steep else self.glVertex_coord(x, y)
+            
+            offset += 2*dy
+
+            if offset >= limit:
+                y += 1 if y0 < y1 else -1
+                limit += 2*dx
 
     def glFinish(self, filename):
         archivo = open(filename, 'wb')
